3708c9d4a12145db837f54d4118d0e262e28d83e
aonemd <aonemdsaleh@gmail.com>
2018-03-28 08:13:17 +0200

    Fix rubocop LineLength offense

--- app/controllers/api/v1/current_weather_controller.rb
+++ app/controllers/api/v1/current_weather_controller.rb
@@ -7,12 +7,14 @@ module Api
 
       def show
         json = if params[:query_type] == 'by_coord'
-                 Rails.cache.fetch("current_weather/by_coord/#{params[:lat]}/#{params[:lon]}", expires_in: 10.minutes) do
+                 Rails.cache.fetch("current_weather/by_coord/#{params[:lat]}/#{params[:lon]}",
+                                   expires_in: 10.minutes) do
                    owm_client.geocode(lat: params[:lat], lon: params[:lon])
                    parse_owm_response(owm_client)
                  end
                else
-                 Rails.cache.fetch("current_weather/by_city/#{params[:city]}/#{params[:country]}", expires_in: 10.minutes) do
+                 Rails.cache.fetch("current_weather/by_city/#{params[:city]}/#{params[:country]}",
+                                   expires_in: 10.minutes) do
                    owm_client.city(city: params[:city], country: params[:country])
                    parse_owm_response(owm_client)
                  end
