3fe73a1ab33e3cc1c044672cffab76c80c01d82a
aonemd <aonemdsaleh@gmail.com>
2018-04-04 12:55:37 +0200

    Add a header

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from 'components/header.vue';
 
 export default {
+  components: {
+    'app-header': header
+  }
 }
 </script>
 
---
+++ frontend/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/" exact>Home</router-link></li>
+      <li><router-link to="/sign-in" exact>Sign In</router-link></li>
+      <li><router-link to="/sign-up" exact>Sign Up</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+nav {
+  padding: 14px 0;
+  margin-bottom: 40px;
+}
+.router-link-active {
+  background: #eee;
+  color: #444;
+}
+</style>
