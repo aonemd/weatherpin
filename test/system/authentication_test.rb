5b1c6a89483ab760821d0b12c507afcf226bb03a
aonemd <aonemdsaleh@gmail.com>
2018-04-04 14:43:07 +0200

    Fix authentication test

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -9,7 +9,8 @@ class AuthenticationTest < ApplicationSystemTestCase
     fill_in 'sign-up__password', with: '12345678'
     click_button 'Sign up'
 
-    assert_selector 'p', 'Signed in as randy'
+    assert_selector 'li', 'randy'
+    assert_selector 'a', 'Sign Out'
   end
 
   test "sign in with email" do
@@ -21,7 +22,8 @@ class AuthenticationTest < ApplicationSystemTestCase
     fill_in 'sign-in__password', with: '12345678'
     click_button 'Sign in'
 
-    assert_selector 'p', "Signed in as #{user.email}"
+    assert_selector 'li', user.username
+    assert_selector 'a', 'Sign Out'
   end
 
   test "sign in with username" do
@@ -33,6 +35,7 @@ class AuthenticationTest < ApplicationSystemTestCase
     fill_in 'sign-in__password', with: '12345678'
     click_button 'Sign in'
 
-    assert_selector 'p', "Signed in as #{user.email}"
+    assert_selector 'li', user.username
+    assert_selector 'a', 'Sign Out'
   end
 end
