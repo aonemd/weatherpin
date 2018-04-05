6b49a31a99a74da1c0d0c1adef6f0f739d235053
aonemd <aonemdsaleh@gmail.com>
2018-04-05 21:05:57 +0200

    Add posts model and db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331175146) do
+ActiveRecord::Schema.define(version: 20180405185009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "temperature", null: false
+    t.string "location_name"
+    t.integer "unit", default: 0, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 20180331175146) do
     t.string "password_digest", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
