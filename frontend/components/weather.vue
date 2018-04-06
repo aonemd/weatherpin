92fd384993d9d0d2f373c512828cbacd2eba3688
aonemd <aonemdsaleh@gmail.com>
2018-04-06 20:40:59 +0200

    Move weatherWidget into `weather` container component

---
+++ frontend/components/weather.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <weather-widget></weather-widget>
+  </div>
+</template>
+
+<script>
+import weatherWidget from './weatherWidget.vue';
+
+export default {
+  components: {
+    'weather-widget': weatherWidget
+  }
+}
+</script>
+
+<style>
+</style>
