48543ce41e162e0f60fee07c51f2b346dc220e5c
aonemd <aonemdsaleh@gmail.com>
2018-04-09 16:39:43 +0200

    Fix failing test due to changing pages routes

--- test/controllers/pages_contrller_test.rb
+++ test/controllers/pages_contrller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get weatjer" do
-    get pages_weather_url
+    get root_url
     assert_response :success
   end
 end
